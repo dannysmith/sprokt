935d0208b8a643dff6c453fbb8296224714210b6
Danny Smith <hi@danny.is>
2020-03-05 02:47:16 +0000

    Order models by created_at
    See https://andycroll.com/ruby/fix-first-last-explicitly-set-implicit-ordering-in-rails-with-uuids/

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  # Since we are using UUIDs as PKs, order by created_at
+  self.implicit_order_column = "created_at"
 end
